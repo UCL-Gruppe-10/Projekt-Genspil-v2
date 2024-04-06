e6cf1cca51186ee1d3d6061e41fb0b01ea5e8edc
anders Holm <holm6792@gmail.com>
2024-04-06 15:19:38 +0200

    SearchGame in the making

--- Projekt Genspil v.2/Menu.cs
+++ Projekt Genspil v.2/Menu.cs
@@ -88,17 +88,99 @@ namespace Projekt_Genspil_v._2
         }
 
         void SearchGames()
-        { 
-            Console.WriteLine("Indstast titlen på det spil du søger efter:");
-            string searchWord = Console.ReadLine();
+        {
+            string searchCriteria;
+            while (true)
+            {
+                Console.Clear(); 
+                Console.WriteLine("Du har valgt søgefunktionen");
+                Console.WriteLine("=================");
+                Console.WriteLine("Søg efter:");
+                Console.WriteLine("(1) Titel");
+                Console.WriteLine("(2) Genre");
+                Console.WriteLine("(3) Spillere");
+                Console.WriteLine("(4) Stand");
+                Console.WriteLine("(5) Pris");
+                Console.WriteLine("(6) Noter");
+                Console.WriteLine("\n(0) Tilbage");
+                Console.WriteLine("=================");
+
+                int searchMenu = int.Parse(Console.ReadLine());
+                switch (searchMenu)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        Console.Write("Søg efter titel: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && searchCriteria.Length > 0)
+                        {
+                            SearchGamesTitle(searchCriteria);
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Søg efter genre: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && searchCriteria.Length > 0)
+                        {
+                            SearchGamesGenre(searchCriteria);
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Søg efter spillere: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && int.Parse(searchCriteria) > 0)
+                        {
+                            SearchGamesPlayers(int.Parse(searchCriteria));
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Søg efter stand: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && searchCriteria.Length == 1)
+                        {
+                            //SearchGamesCondition(Convert.ToChar(searchCriteria));
+                        }
+                        break;
+                    case 5:
+                        Console.Write("Søg efter pris: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && int.Parse(searchCriteria) > 0)
+                        {
+                            //SearchGamesPrice(int.Parse(searchCriteria));
+                        }
+                        break;
+                    case 6:
+                        Console.Write("Søg efter noter: ");
+                        searchCriteria = Console.ReadLine();
+                        if (searchCriteria != null && searchCriteria.Length > 0)
+                        {
+                            //SearchGamesNotes(searchCriteria);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Søgekriterie ikke brugbart.\nTryk Enter for at prøve igen.");
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+
+        }
+
+        void SearchGamesTitle(string searchWord)
+        {
 
             Console.WriteLine($"Resultatet af din søgning: '{searchWord}'");
-            bool found = false; 
+            bool found = false;
+            //int indexerTitle;
             foreach (Game game in gameTitle) // Her gennemgår den Array'et efter søgeordet, hvor den så vil sortere dem efter kriteriet
             {
+                //indexerTitle++;
                 if (game != null && game.title.IndexOf(searchWord, StringComparison.OrdinalIgnoreCase) >= 0)
                 {
-                    Console.WriteLine(game.title);
+                    Console.WriteLine($"{game.title}");
+                    //Console.WriteLine($"{game.title} ({indexerTitle})");
                     found = true;
                 }
             }
@@ -109,8 +191,101 @@ namespace Projekt_Genspil_v._2
             }
 
             Console.ReadLine();
+        }
+
+        void SearchGamesGenre(string searchWord)
+        {
+            Console.WriteLine($"Resultatet af din søgning: '{searchWord}'");
+            bool found = false;
+            foreach(Game game in gameTitle)
+            {
+                if(game != null && game.title.IndexOf(searchWord,StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    Console.WriteLine($"{game.title}, {game.genre}");
+                    found = true;
+                }
+            }
+
+        if(!found)
+            {
+                Console.WriteLine($"Ingen søgeresultater fundet for '{searchWord}'");
+            }
+            Console.ReadLine();
 
         }
+
+        void SearchGamesPlayers(int searchWord)
+        {
+
+            Console.WriteLine($"Resultatet af din søgning: '{searchWord}'");
+            bool found = false;
+            foreach (Game game in gameTitle) // Her gennemgår den Array'et efter søgeordet, hvor den så vil sortere dem efter kriteriet
+            {
+                if (game != null && game.players[0] < searchWord && game.players[1] > searchWord)
+                {
+                    Console.WriteLine($"{game.title}, {game.players[0]} til {game.players[1]}");
+                    found = true;
+                }
+            }
+            // Vis en besked, hvis der ikke er nogen søgeresultater
+            if (!found)
+            {
+                Console.WriteLine($"Ingen søgeresultater fundet for '{searchWord}'");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        //void SearchGamesCondition(string searchWord)
+        //{
+        //    Console.WriteLine($"Resultatet af din søgning: '{searchWord}'");
+        //    bool found = false;
+        //    foreach (Game game in gameTitle)
+        //    {
+        //        if (game != null && game.title.IndexOf(searchWord, StringComparison.OrdinalIgnoreCase) >= 0)
+        //        {
+        //            Console.WriteLine($"{game.title}, {game.gameCondiditon}");
+        //            found = true;
+        //        }
+        //    }
+
+        //    if (!found)
+        //    {
+        //        Console.WriteLine($"Ingen søgeresultater fundet for '{searchWord}'");
+        //    }
+        //    Console.ReadLine();
+
+        //}
+
+        //void SearchGamesPrice(int searchWord)
+        //{
+
+        //    Console.WriteLine($"Resultatet af din søgning: '{searchWord}'");
+        //    bool found = false;
+        //    foreach (Game game in gameTitle) // Her gennemgår den Array'et efter søgeordet, hvor den så vil sortere dem efter kriteriet
+        //    {
+        //        if (game != null && game.price[i] < searchWord)
+        //        {
+        //            Console.WriteLine($"{game.title}, {game.players[0]} til {game.players[1]}");
+        //            found = true;
+        //        }
+        //    }
+        //    // Vis en besked, hvis der ikke er nogen søgeresultater
+        //    if (!found)
+        //    {
+        //        Console.WriteLine($"Ingen søgeresultater fundet for '{searchWord}'");
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+
+
+
+
+
+
 
         void CreateGame()
         {
